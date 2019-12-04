ae256c37ab56a31a2ecefd022862a7b99d034655
Lander Azkona Bengoa <EXTlazkona@oesia.com>
2019-12-04 14:53:36 +0100

    Routing completo

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService }  from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -12,9 +16,23 @@ export class HeroDetailComponent implements OnInit {
     id: 121,
     name: 'Superper'
   };
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
   ngOnInit() {
+    this.getHero();
+  }
+  
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id).subscribe( heroeRecibido => this.hero = heroeRecibido);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,14 +8,8 @@ import { HeroService } from "../hero.service";
   styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
 
   heroes: Hero[];
-
-  onSelect(hero: Hero) {
-    console.log("onSelect", hero);
-    this.selectedHero = hero;
-  }
 
   constructor(private heroService: HeroService) {}
 
